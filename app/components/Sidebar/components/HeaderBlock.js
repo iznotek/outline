26185df0b136a0ce2a07cc5745224965f9ee1937
Pierre Leduc <pierre.leduc@ifeelsmart.com>
2020-03-24 18:08:11 +0100

    TEAM_LOGO in webpack finally

--- app/components/Sidebar/components/HeaderBlock.js
+++ app/components/Sidebar/components/HeaderBlock.js
@@ -23,7 +23,7 @@ function HeaderBlock({
 }: Props) {
   return (
     <Header justify="flex-start" align="center" {...rest}>
-      <TeamLogo alt={`${teamName} logo`} src={logoUrl || process.env.TEAM_LOGO} />
+      <TeamLogo alt={`${teamName} logo`} src={logoUrl} />
       <Flex align="flex-start" column>
         <TeamName showDisclosure>
           {teamName}{' '}
