c0f481ea5c70b13bce4ba1aab7bd8afd37d73fe6
Pierre Leduc <pierre.leduc@ifeelsmart.com>
2020-03-24 11:40:42 +0100

    Add PEERTUBE_ALLOWED_DOMAINS optional env var for security

--- app/embeds/PeerTube.js
+++ app/embeds/PeerTube.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import Frame from './components/Frame';
 
 const URL_REGEX = /(?:https?:\/\/)?(?:www\.)?\/?.*?\/videos\/(?:watch|embed)?(?:.*v=|v\/|\/)([a-zA-Z0-9_-]{36})$/i;
+const allowedDomainsEnv = process.env.PEERTUBE_ALLOWED_DOMAINS;
 
 type Props = {
   url: string,
@@ -15,11 +16,19 @@ export default class PeerTube extends React.Component<Props> {
   render() {
     const { matches } = this.props;
     const url = matches[0];
+    const normalizedUrl = url.replace('watch', 'embed');
+
+    const allowedDomains = allowedDomainsEnv && allowedDomainsEnv.split(',');
+    if (allowedDomains) {
+      if (allowedDomains.findIndex(domain => normalizedUrl.includes(domain)) < 0) {
+        return null;
+      }
+    }
 
     return (
       <Frame
-        src={`${url}`}
-        title={`PeerTube (${url})`}
+        src={`${normalizedUrl}`}
+        title={`PeerTube (${normalizedUrl})`}
       />
     );
   }
